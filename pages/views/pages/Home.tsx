f36f82de2a2ed7d138692db603e5fa94550a2f59
Tomislav Fabeta <tomislavfabeta@rentalcars.com>
2019-04-28 21:50:17 +0100

    Added social images in column.

--- pages/views/pages/Home.tsx
+++ pages/views/pages/Home.tsx
@@ -17,10 +17,13 @@ import {
     SearchableDropdownObservable,
     SearchableDropdown,
     DropdownItemDetails,
+
+    SocialImages,
+    SocialImagesViewObject,
+    SocialImagesProps,
 } from '@snoutyfriend/storybook';
 import {NextContext} from 'next';
 import Router from 'next/router';
-// import Router from 'next-server/router';
 
 const style = require('@snoutyfriend/storybook/scss/all.scss');
 const homeStyle = require('../../../scss/pages/home/home.scss');
@@ -48,7 +51,7 @@ export default class Home extends React.Component<HomeProps, {}> {
 
     public render() {
         const {formConfiguration, headerConfiguration} = this.props;
-        const type = FindPlacesHeaderType.FIXED;
+        const type = FindPlacesHeaderType.DEFAULT;
         const viewObject = headerConfiguration.viewObject;
 
         const staticUrl = '/static';
@@ -63,6 +66,7 @@ export default class Home extends React.Component<HomeProps, {}> {
         searchableDropdownObservable.getObservableActiveItem().subscribe((activeItem: DropdownItemDetails) => {
             this.dropdownActiveItem = activeItem;
         });
+
         observableForm.getSubmittedObservable().subscribe(() => {
             if (this.dropdownActiveItem === undefined) {
                 return;
@@ -74,15 +78,46 @@ export default class Home extends React.Component<HomeProps, {}> {
             });
         });
 
+        const socialImagesViewObject: SocialImagesViewObject = {
+            title: 'Pridruzite se Snoutyfriend zajednici na',
+            links: [
+                {
+                    href: '',
+                    value: 'Facebooku',
+                },
+                {
+                    href: '',
+                    value: 'Instagramu',
+                },
+            ],
+            andSeparator: 'i',
+            images: [
+                'https://scontent-lht6-1.cdninstagram.com/vp/7c60c7669f848fffcb3a0da8847a4812/5D5703B7/t51.2885-15/sh0.08/e35/s640x640/56962154_2269904143268101_3706237357547553237_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
+                'https://scontent-lht6-1.cdninstagram.com/vp/ba2bf9063bd537326dc2cacef449972c/5D5AB71C/t51.2885-15/e35/54513663_585604535283742_4348662001913804805_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
+                'https://scontent-lht6-1.cdninstagram.com/vp/7c60c7669f848fffcb3a0da8847a4812/5D5703B7/t51.2885-15/sh0.08/e35/s640x640/56962154_2269904143268101_3706237357547553237_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
+                'https://scontent-lht6-1.cdninstagram.com/vp/7c60c7669f848fffcb3a0da8847a4812/5D5703B7/t51.2885-15/sh0.08/e35/s640x640/56962154_2269904143268101_3706237357547553237_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
+                'https://scontent-lht6-1.cdninstagram.com/vp/7c60c7669f848fffcb3a0da8847a4812/5D5703B7/t51.2885-15/sh0.08/e35/s640x640/56962154_2269904143268101_3706237357547553237_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
+                'https://scontent-lht6-1.cdninstagram.com/vp/ba2bf9063bd537326dc2cacef449972c/5D5AB71C/t51.2885-15/e35/54513663_585604535283742_4348662001913804805_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
+                'https://scontent-lht6-1.cdninstagram.com/vp/7c60c7669f848fffcb3a0da8847a4812/5D5703B7/t51.2885-15/sh0.08/e35/s640x640/56962154_2269904143268101_3706237357547553237_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
+            ],
+        };
+
         return (
             <div>
                 <FindPlacesHeader viewObject={viewObject} type={type} imagesRepository={imagesRepository}/>
-                <div className='search-form'>
-                    <FindPlacesForm
-                        observableForm={observableForm}
-                        viewObject={findPlacesViewObject}
-                        searchableDropdownObservable={searchableDropdownObservable}
-                        imagesRepository={imagesRepository} />
+                <div className='col'>
+                    <div className='home-search-form'>
+                        <FindPlacesForm
+                            observableForm={observableForm}
+                            viewObject={findPlacesViewObject}
+                            searchableDropdownObservable={searchableDropdownObservable}
+                            imagesRepository={imagesRepository} />
+                    </div>
+                </div>
+                <div className='col'>
+                    <div className='home-social-images'>
+                        <SocialImages viewObject={socialImagesViewObject} />
+                    </div>
                 </div>
             </div>
         );
